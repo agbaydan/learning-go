a0a2d3315255e7619885ead5fc082cafd39c1523
daniel <danielagbay@gmail.com>
2022-03-01 12:33:48 -0500

    added random greetings

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	
 	"errors"
+
+	"math/rand"
+
+	"time"
 )
 
 func Hello(name string) (string, error) {
@@ -15,8 +19,27 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	
 	// nil means no error aka empty value
 	return message, nil
+}
+
+// https://go.dev/doc/effective_go#init
+// Go calls init functions at program startup after global vars have been initialized
+// so kinda like a ctor for this file
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// starting with lowercase means this method is essentially internal.
+// it is not exported and not accessible by other packages
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"You and I could rule this city, %v. Or, we could fight to the death!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
